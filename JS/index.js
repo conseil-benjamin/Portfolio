4b3d1503cac191dcec8adeb7834ba32b3190c881
conseil-benjamin <benlenormand61@gmail.com>
2023-06-30 02:17:40 +0200

    Modification section competences et profil

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,21 @@
 // permet de retourner en arrière dans l'historique, donc à la page d'avant
 document.getElementById("go-back").addEventListener("click", () => {
-    history.back();
+  history.back();
 });
+
+function filterSkills() {
+  var categorySelect = document.getElementById("category-select");
+  var selectedCategory = categorySelect.value;
+  var skillBars = document.getElementsByClassName("skill-bar");
+
+  for (var i = 0; i < skillBars.length; i++) {
+    var skillBar = skillBars[i];
+    var category = skillBar.getAttribute("data-category");
+
+    if (category === selectedCategory) {
+      skillBar.style.display = "block"; // Afficher la compétence
+    } else {
+      skillBar.style.display = "none"; // Masquer la compétence
+    }
+  }
+}
