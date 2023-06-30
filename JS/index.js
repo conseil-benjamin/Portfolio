da0864429da2f64a2183dc093451d4e5507bc465
conseil-benjamin <benlenormand61@gmail.com>
2023-07-01 00:51:07 +0200

    01-07-23

--- JS/index.js
+++ JS/index.js
@@ -8,6 +8,18 @@ function filterSkills() {
   var selectedCategory = categorySelect.value;
   var skillBars = document.getElementsByClassName("skill-bar");
 
+
+  // marche pas du tout / faire en sorte que par défaut les comp. web soit affichés
+  for (var i = 0; i < skillBars.length; i++) {
+    var skillBar = skillBars[i];
+    var category = skillBar.getAttribute("data-category");
+    if(selectedCategory === "web"){
+      if(category === selectedCategory){
+        skillBar.style.display = "block";
+      }
+    }
+  }
+  
   for (var i = 0; i < skillBars.length; i++) {
     var skillBar = skillBars[i];
     var category = skillBar.getAttribute("data-category");
